5c589c8e273faebac3a9e43c0954473ba1b03887
tahreem_rasul <rasul.tahreem1@gmail.com>
2024-03-28 02:12:21 +0500

    added literal prompt functionality

--- search_engine.py
+++ search_engine.py
@@ -4,17 +4,24 @@ from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQA
-from langchain_openai import OpenAI
+from langchain_openai import ChatOpenAI
+from literalai import LiteralClient
 from dotenv import load_dotenv
 
 load_dotenv()
+
+client = LiteralClient()
+
+# This will fetch the champion version, you can also pass a specific version
+prompt = client.api.get_prompt_sync(name="test_prompt")
+prompt = prompt.to_langchain_chat_prompt_template()
 
 
 @cl.on_chat_start
 async def retrieve_docs():
     if cl.context.session.client_type == "copilot":
-        llm = OpenAI(model='gpt-3.5-turbo-instruct',
-                     temperature=0)
+        llm = ChatOpenAI(model='gpt-3.5-turbo',
+                         temperature=0)
 
         # QUERY PORTION
         query = None
@@ -53,11 +60,19 @@ async def retrieve_docs():
         db = Chroma.from_documents(pdf_data[0], embeddings)
 
         # CHAIN PORTION
-        chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=db.as_retriever())
+        chain = RetrievalQA.from_chain_type(llm=llm,
+                                            chain_type="stuff",
+                                            retriever=db.as_retriever(),
+                                            chain_type_kwargs={
+                                                "verbose": True,
+                                                "prompt": prompt
+                                            }
+                                            )
 
         # Let the user know that the system is ready
         await cl.Message(content=f"Database creation for `{arxiv_query}` complete. You can now ask questions!").send()
 
+        cl.user_session.set("db", db)
         cl.user_session.set("chain", chain)
 
 
@@ -66,7 +81,12 @@ async def retrieve_docs(message: cl.Message):
     if cl.context.session.client_type == "copilot":
         question = message.content
         chain = cl.user_session.get("chain")
-        database_results = await chain.acall({"query": question}, callbacks=[cl.AsyncLangchainCallbackHandler()])
+        db = cl.user_session.get("db")
+        # Create a new instance of the callback handler for each invocation
+        cb = client.langchain_callback()
+        variables = {"context": db.as_retriever(search_kwargs={"k": 1}), "query": question}
+        database_results = await chain.acall(variables,
+                                             callbacks=[cb])
         results = [f"Question: {question} \n Answer: {database_results['result']}"]
         # Trigger popup for database results
         fn_db = cl.CopilotFunction(name="showDatabaseResults", args={"results": "\n".join(results)})
